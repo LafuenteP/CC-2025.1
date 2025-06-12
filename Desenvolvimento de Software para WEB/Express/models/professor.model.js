8023027e559d550c9d03c3e3c808679fd5c00aab
Lafuente Paulino <lafuente12erty@gmail.com>
2025-06-12 13:57:17 -0300

    novas_aulas

---
+++ Desenvolvimento de Software para WEB/Express/models/professor.model.js
@@ -0,0 +1,13 @@
+class ProfessorModel {
+
+    constructor(){
+        this.id = id;
+        this.nome = nome;
+        this.titulacao = titulacao;
+        this.img = img;
+    }
+}
+
+
+
+module.exports = ProfessorModel
